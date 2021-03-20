b7fa39154f4a7c233a2490f767fe8274da6d6eca
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-20 17:24:48 -0400

    init Controller for Wdiget Service

--- src/main/java/com/example/wbdvsp2103jiahaocaiserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2103jiahaocaiserverjava/controllers/WidgetController.java
@@ -1,2 +1,37 @@
-package com.example.wbdvsp2103jiahaocaiserverjava.controllers;public class WidgetController {
+package com.example.wbdvsp2103jiahaocaiserverjava.controllers;
+
+import com.example.wbdvsp2103jiahaocaiserverjava.models.Widget;
+import com.example.wbdvsp2103jiahaocaiserverjava.services.WidgetService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+    @Autowired
+    WidgetService service;
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String topicId
+    ) {
+        return service.findAllWidgetsForTopic(topicId);
+    }
+
+    @GetMapping("/api/widgets/{wid}")
+    public Widget findWidgetById(
+            @PathVariable("wid") Long id) {
+        return service.findWidgetById(id);
+    }
 }
