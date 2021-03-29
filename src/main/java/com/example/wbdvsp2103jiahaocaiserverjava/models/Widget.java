3c7a4d825b8fa2bd349b5f64d439678bfab1f630
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 01:06:02 -0400

    add necessary attributes, rearrange the attributes, add a copy method for updating a Widget

--- src/main/java/com/example/wbdvsp2103jiahaocaiserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp2103jiahaocaiserverjava/models/Widget.java
@@ -1,23 +1,67 @@
 package com.example.wbdvsp2103jiahaocaiserverjava.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="widgets")
 public class Widget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String topicId;
+    private String name;
     private String type;
+//    private Integer widgetOrder;
+    private String text;
+    private String src;
     private Integer size;
     private Integer width;
     private Integer height;
-    private String text;
-    private String topicId;
 
     public Widget() {
     }
 
-    public Widget(Long id, String topicId, String type, Integer size, String text) {
-      this.id = id;
-      this.topicId = topicId;
-      this.type = type;
-      this.size = size;
-      this.text = text;
+    public Widget(Widget other) {
+        this.id = other.id;
+        this.topicId = other.topicId;
+        this.name = other.name;
+        this.type = other.type;
+        this.text = other.text;
+        this.src = other.src;
+        this.size = other.size;
+        this.width = other.width;
+        this.height = other.height;
+    }
+
+    public void copyWidget(Widget other) {
+        this.id = other.id;
+        this.topicId = other.topicId;
+        this.name = other.name;
+        this.type = other.type;
+        this.text = other.text;
+        this.src = other.src;
+        this.size = other.size;
+        this.width = other.width;
+        this.height = other.height;
+    }
+
+
+    public Widget(Long id, String topicId, String name, String type,
+                  String text, String src, Integer size, Integer width, Integer height) {
+        this.id = id;
+        this.topicId = topicId;
+        this.name = name;
+        this.type = type;
+        this.text = text;
+        this.src = src;
+        this.size = size;
+        this.width = width;
+        this.height = height;
     }
 
     public Long getId() {
@@ -28,20 +72,28 @@ public class Widget {
       this.id = id;
     }
 
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getType() {
       return type;
     }
 
     public void setType(String type) {
       this.type = type;
-    }
-
-    public Integer getSize() {
-      return size;
-    }
-
-    public void setSize(Integer size) {
-      this.size = size;
     }
 
     public String getText() {
@@ -52,12 +104,20 @@ public class Widget {
       this.text = text;
     }
 
-    public String getTopicId() {
-      return topicId;
+    public String getSrc() {
+        return src;
     }
 
-    public void setTopicId(String topicId) {
-      this.topicId = topicId;
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
     }
 
     public Integer getWidth() {
@@ -75,4 +135,33 @@ public class Widget {
     public void setHeight(Integer height) {
         this.height = height;
     }
+
 }
+
+/*
+
+    create table widgets (
+       id bigint not null auto_increment,
+        height integer,
+        size integer,
+        text varchar(255),
+        topic_id varchar(255),
+        type varchar(255),
+        width integer,
+        primary key (id)
+    ) engine=InnoDB
+
+    alter table widgets
+       add column name varchar(255)
+
+    create table widget (
+       id bigint not null,
+        height integer,
+        size integer,
+        text varchar(255),
+        topic_id varchar(255),
+        type varchar(255),
+        width integer,
+        primary key (id)
+    ) engine=InnoDB
+ */
