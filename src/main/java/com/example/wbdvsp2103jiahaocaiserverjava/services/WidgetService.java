65af87e6d5b33270f19f628bed3eca4b0d8bfc50
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 01:08:11 -0400

    remove the dummy list and use repository.

--- src/main/java/com/example/wbdvsp2103jiahaocaiserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2103jiahaocaiserverjava/services/WidgetService.java
@@ -1,80 +1,47 @@
 package com.example.wbdvsp2103jiahaocaiserverjava.services;
 
 import com.example.wbdvsp2103jiahaocaiserverjava.models.Widget;
+import com.example.wbdvsp2103jiahaocaiserverjava.repositories.WidgetRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class WidgetService {
-    private List<Widget> widgets = new ArrayList<Widget>();
-    {
-      Widget w1 = new Widget(123L, "60483c3f104b9800172019b3", "HEADING", 1, "Welcome to Widgets.");
-      Widget w2 = new Widget(234l, "60483c3f104b9800172019b3", "PARAGRAPH", 1, "Lorem Ipsum");
-      Widget w3 = new Widget(345L, "60496a19ecea170017b5831e", "PARAGRAPH", 1, "This is a paragraph.");
-      Widget w4 = new Widget(456L, "60496a19ecea170017b5831e", "HEADING", 2, "Welcome to WebDev.");
-      Widget w5 = new Widget(567L, "60496a19ecea170017b5831e", "PARAGRAPH", 1, "Lorem ipsum.");
-      widgets.add(w1);
-      widgets.add(w2);
-      widgets.add(w3);
-      widgets.add(w4);
-      widgets.add(w5);
-    }
+
+    @Autowired
+    WidgetRepository repository;
 
     // implement crud operations
-    public Widget createWidgetForTopic(String topicId, Widget widget) {
-      Long id = (new Date()).getTime();
-      widget.setId(id);
-      widget.setTopicId(topicId);
-      widgets.add(widget);
-      return widget;
+    public Widget createWidgetForTopic(Widget widget) {
+        return repository.save(widget);
     }
 
     public List<Widget> findAllWidgets() {
-      return widgets;
+        return repository.findAllWidgets();
     }
 
     public List<Widget> findAllWidgetsForTopic(String topicId) {
-        List<Widget> ws = new ArrayList<Widget>();
-        for (Widget w : widgets) {
-            if (w.getTopicId().equals(topicId)) {
-              ws.add(w);
-            }
-        }
-      return ws;
+        return repository.findAllWidgetsForTopic(topicId);
     }
 
     public Widget findWidgetById(Long id) {
-      for (Widget w : widgets) {
-        if (w.getId().equals(id)) {
-          return w;
-        }
-      }
-      return null;
+        return repository.findWidgetById(id);
     }
 
     public Integer updateWidget(Long id, Widget widget) {
-        for (int i=0; i < widgets.size(); i++) {
-            if (widgets.get(i).getId().equals(id)) {
-                widgets.set(i, widget);
-                return 1;
-            }
-        }
-        return -1;
+        Widget originalWidget = repository.findWidgetById(id);
+        // copy constructor!!!
+        originalWidget.copyWidget(widget);
+        repository.save(originalWidget);
+        return 1;
     }
 
     public Integer deleteWidget(Long id) {
-      int index = -1;
-      for (int i=0; i < widgets.size(); i++) {
-          if (widgets.get(i).getId().equals(id)) {
-              index = i;
-              widgets.remove(index);
-              return -1;
-          }
-      }
-      return 1;
+        repository.deleteById(id);
+        return 1;
     }
 
 }
